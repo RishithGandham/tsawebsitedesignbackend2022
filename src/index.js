2b661bd4a74e4e5eba0769ece115ec41e8555192
RishithGandham <>
2022-03-14 15:17:01 +0000

    started backend

---
+++ src/index.js
@@ -0,0 +1,3 @@
+
+const {listen, app} = require('./server.js')
+listen();
---
+++ src/server.js
@@ -0,0 +1,16 @@
+
+const dotenv = require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 5000
+
+const  listen = () => {
+  app.listen(port, () => {
+    console.log('app listening on port:', port);
+  })
+}
+
+module.exports = {
+  app,
+  listen
+}
