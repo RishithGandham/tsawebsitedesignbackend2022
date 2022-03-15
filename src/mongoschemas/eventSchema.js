f662e668cc8b83dd0a0845484e4217bc1f716dc5
RishithGandham <rishith.gandham@gmail.com>
2022-03-14 22:15:49 -0400

    created event schema

---
+++ src/mongoschemas/eventSchema.js
@@ -0,0 +1,25 @@
+
+const mongoose = require('mongoose');
+
+
+const eventSchema = mongoose.Schema( {
+    name: {
+        type: String,
+        required: 'this feild is required'
+    },
+    date: {
+        type: String,
+        required: 'this feild is required'
+    },
+    maxTickets: {
+        type: Number,
+        required: 'this feild is required'
+    },
+    ticketsSold: {
+        type: Number,
+        required: 'this feild is required'
+    }
+})
+
+module.exports = mongoose.model('Event', eventSchema);
+
