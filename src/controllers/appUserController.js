898871369db2472e67e99e9a6250946ee44025bc
RishithGandham <rishith.gandham@gmail.com>
2022-03-16 22:27:15 -0400

    proper jwt authentication

--- src/controllers/appUserController.js
+++ src/controllers/appUserController.js
@@ -1,105 +1,105 @@
+//dot env
+require('dotenv').config();
 
-
+//express
 const express = require('express');
-const mongoose = require('mongoose');
 const router = express.Router();
-const AppUser = mongoose.model('AppUser');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
+
+//mongoose
+const mongoose = require('mongoose');
+const AppUser = mongoose.model('AppUser');
 
 //middle ware
 const jwtmiddleware = require('../middleware/jwtMiddleWare');
 
 router.post('/login', async (req, res) => {
-   try {
-    // Get user input
-    const { email, password } = req.body;
+    try {
+        // Get user input
+        const { email, password } = req.body;
 
-    // Validate user input
-    if (!(email && password)) {
-      res.status(400).send("All input is required");
-    }
-    // Validate if user exist in our database
-    const user = await AppUser.findOne({ email });
-
-    if (user && (await bcrypt.compare(password, user.password))) {
-      // Create token
-      const token = jwt.sign(
-        { user_id: user._id, email },
-        '/5oH=fl*RVKvSR#Pz$Gu@2@jk6~anzb_T+JD"8<W#pp_yIy}#d9x&&Amo8c:f',
-        {
-          expiresIn: "2h",
+        // Validate user input
+        if (!(email && password)) {
+            return res.status(400).send('All input is required');
         }
-      );
+        // Validate if user exist in our database
+        const user = await AppUser.findOne({ email });
 
-      // user
-      res.status(200).json({
-        jwt: token, 
-        user: user
-      });
+        if (user && (await bcrypt.compare(password, user.password))) {
+            // Create token
+            const token = jwt.sign(
+                { user_id: user._id, email },
+                process.env.TOKEN_KEY,
+                {
+                    expiresIn: '2h',
+                }
+            );
+
+            // user
+            return res.status(200).json({
+                jwt: token,
+                user: user,
+            });
+        }
+        return res.status(400).send('Invalid Credentials');
+    } catch (err) {
+        res.status(400).send('Invalid Credentials');
+        console.log(err);
     }
-    res.status(400).send("Invalid Credentials");
-  } catch (err) {
-    res.status(400).send("Invalid Credentials");
-    console.log(err);
-  }
 });
 
 router.post('/register', async (req, res) => {
     try {
+        const { firstName, lastName, email, password } = req.body;
 
-      const { firstName, lastName, email, password } = req.body;
+        console.log(req.body);
 
-      console.log(req.body);
-  
-      if (!(email && password && firstName && lastName)) {
-        res.status(400).send("All input is required");
-      }
-  
-      
-      const oldUser = await AppUser.findOne({ email });
-  
-      if (oldUser) {
-        return res.status(409).send("User Already Exist. Please Login");
-        
-      }
-  
-      //Encrypt user password
-      encryptedPassword = await bcrypt.hash(password, 10);
-  
-      // Create user in our database
-      const user = await AppUser.create({
-        firstName,
-        lastName,
-        email: email, // sanitize: convert email to lowercase
-        password: encryptedPassword,
-      });
-  
-      // Create token
-      const token = jwt.sign(
-        { user_id: user._id, email },
-        '/5oH=fl*RVKvSR#Pz$Gu@2@jk6~anzb_T+JD"8<W#pp_yIy}#d9x&&Amo8c:f',
-        {
-          expiresIn: "2h",
+        if (!(email && password && firstName && lastName)) {
+            return res.status(400).send('All input is required');
         }
-      );
-      
-  
-      // return new user
-      res.status(201).json({
-        jwt: token,
-        user: user
-      });
-      } catch (error) {
-          res.send('error saving user');
-          console.log(error);
-      }
+
+        const oldUser = await AppUser.findOne({ email });
+
+        if (oldUser) {
+            return res.status(409).send('User Already Exist. Please Login');
+        }
+
+        //Encrypt user password
+        encryptedPassword = await bcrypt.hash(password, 10);
+
+        // Create user in our database
+        const user = await AppUser.create({
+            firstName,
+            lastName,
+            email: email, // sanitize: convert email to lowercase
+            password: encryptedPassword,
+        });
+
+        // Create token
+        const token = jwt.sign(
+            { user_id: user._id, email },
+            process.env.TOKEN_KEY,
+            {
+                expiresIn: '2h',
+            }
+        );
+
+        // return new user
+        return res.status(201).json({
+            jwt: token,
+            user: user,
+        });
+    } catch (error) {
+        res.send('error saving user');
+        console.log(error);
+    }
 });
 
-router.post('/update', (req, res) => {
-      
-});
+router.post('/update', (req, res) => {});
 
-router.get('/superPrivateResource', jwtmiddleware,  (req, res) => {});
+router.get('/superPrivateResource', jwtmiddleware, (req, res) => {
+    res.send('hacked into the main frame >:)');
+});
 
 module.exports = router;
